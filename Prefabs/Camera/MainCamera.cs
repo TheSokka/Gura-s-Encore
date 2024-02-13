7bff13b90584ea7f0c3fb7fc30fb5687394c3136
Joel Smith <59793089+TheSokka@users.noreply.github.com>
2024-02-13 00:24:12 -0700

    Update player and camera movements, and add input actions
    This commit primarily updates the player and camera movement and refactor the corresponding scripts for better control. It also introduces new input actions for player control. Other minor updates include changes to project files for .NET package management, and some other undisclosed settings pertaining to Godot's internal operation.

--- Prefabs/Camera/MainCamera.cs
+++ Prefabs/Camera/MainCamera.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class MainCamera : Node {
 	private Camera3D _camera3D;
-	private float _mouseSensitivity = .1f;
+	private float _mouseSensitivity = .2f;
 	private Vector3 _cameraRotationDegrees;
 
 	// Called when the node enters the scene tree for the first time.
@@ -14,15 +14,17 @@ public partial class MainCamera : Node {
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) {
-		_cameraRotationDegrees.X = Mathf.Clamp(_cameraRotationDegrees.X, -75, 75);
-		_camera3D.Call("set_third_person_rotation_degrees", _cameraRotationDegrees);
 	}
 
 	public override void _Input(InputEvent @event) {
 		if (@event is InputEventMouseMotion mouseInput) {
 			GD.Print(_cameraRotationDegrees);
-			_cameraRotationDegrees.X += -mouseInput.Relative.Y * _mouseSensitivity;
-			_cameraRotationDegrees.Y += mouseInput.Relative.X * _mouseSensitivity;
+			_cameraRotationDegrees.X += Mathf.DegToRad(-mouseInput.Relative.Y * _mouseSensitivity);
+			_cameraRotationDegrees.Y += Mathf.DegToRad(mouseInput.Relative.X * _mouseSensitivity);
+			_cameraRotationDegrees.X = Mathf.Clamp(_cameraRotationDegrees.X, -1.11f, 1.11f);
+
+			_camera3D.Call("set_third_person_rotation_degrees", _cameraRotationDegrees);
+
 		}
 	}
 }
--- Prefabs/Player/gura.cs
+++ Prefabs/Player/gura.cs
@@ -1,13 +1,22 @@
 using Godot;
 using System;
 
-public partial class gura : CharacterBody3D
-{
+public partial class gura : CharacterBody3D {
+	private Node3D _visual;
+	private Vector3 _visualRotationDegrees;
+    private float _mouseSensitivity = .2f;
+	
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
-
+	
+	
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	private Vector3 _forwardMovRotation;
+
+	public override void _Ready() {
+		_visual = GetNode<Node3D>("Visual");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -18,15 +27,24 @@ public partial class gura : CharacterBody3D
 			velocity.Y -= gravity * (float)delta;
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("Jump") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
+		
+		float lerpFactor = (float) delta * Speed;
+		
+		if (direction != Vector3.Zero) {
+			var targetDirection = direction.Normalized();
+			var targetAngle = Mathf.Atan2(targetDirection.X, targetDirection.Z);
+
+			var newAngle = Mathf.LerpAngle(Rotation.Y, Mathf.Atan2(velocity.X, velocity.Y), 10f * delta);
+
+			_visual.Rotation = new Vector3(_visual.Rotation.X, (float)newAngle, _visual.Rotation.Z);
+			// Apply the new basis to the _visual
 			velocity.X = direction.X * Speed;
 			velocity.Z = direction.Z * Speed;
 		}
@@ -35,12 +53,14 @@ public partial class gura : CharacterBody3D
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
 		}
-
-		var normalizedVelocity = velocity.Normalized();
-		if (velocity != Vector3.Zero) {
-			var target = GlobalTransform.Origin + new Vector3(direction.X, 0, direction.Y);
-			LookAt(target, new Vector3(0,1,0));		}
+		
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+	public override void _Input(InputEvent @event) {
+		if (@event is InputEventMouseMotion mouseInput) {
+			_visual.RotateY(Mathf.DegToRad(-mouseInput.Relative.X * _mouseSensitivity));
+			RotateY(Mathf.DegToRad(mouseInput.Relative.X * _mouseSensitivity));
+		}
 	}
 }
